820e02b00d5e0dea5e201f9e297542f84779bd79
Wanahyunn <143162467+NurulShafwana@users.noreply.github.com>
2025-06-03 08:52:38 +0800

    RData
    The surveys data are too large. Hence, using RData file

---
+++ survey_monkey_rcodes.R
@@ -0,0 +1,5 @@
+##============================================================================##
+##    Making .RData file for AITI survey monkey
+##    Individual Surveys from each households
+##    Collecting data for ict digital skills
+
