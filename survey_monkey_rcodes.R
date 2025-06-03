2c3166a8b7ad48d441111d51be160c0b7a7dd754
Wanahyunn <143162467+NurulShafwana@users.noreply.github.com>
2025-06-03 16:04:13 +0800

    Update survey_monkey_rcodes.R

--- survey_monkey_rcodes.R
+++ survey_monkey_rcodes.R
@@ -15,7 +15,7 @@ library(survey)
 # Exerting excel data into R
 # Calling it Digital Skills Data
 # Before doing this, make sure the excel is in the file!
-digital_skills <- read_excel("AITI/AITIsurveymonkey/Survey_Data_testest.xlsx")
+digital_skills <- read_excel("AITI/AITIsurveymonkey/Brunei ICT Survey 2024.xlsx")
 
 # To see if its the correct data sets
 head(digital_skills)
@@ -26,9 +26,9 @@ head(digital_skills)
 # Cluster - grouped data (urban rural?)
 # Strata - grouped data to improve representatives
 # The reason of using nest is because there're still some errors/missings in data
-design_ds <- svydesign(ids = ~Gender, strata = ~Area , data = digital_skills,
+design_ds <- svydesign(ids = ~HH, strata = ~GEN , data = digital_skills,
                        nest = TRUE)
-head(design_ds, 10)
+
 
 # calculating the mean of ??
 svymean(~Gender, design_ds)
